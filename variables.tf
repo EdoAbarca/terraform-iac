330c2f462e96c38e734ff81cacdcbe2859412b90
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-07-11 23:04:22 -0400

    build: Se agregan opciones de autenticación con clave pública y secreta AWS

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "key_name" {
     description = "Nombre de la clave SSH"
     type = string
 }
+
+variable "aws_access_key" {
+    description = "Clave de acceso de AWS"
+    type        = string
+}
+
+variable "aws_secret_key" {
+    description = "Clave secreta de AWS"
+    type        = string
+}
